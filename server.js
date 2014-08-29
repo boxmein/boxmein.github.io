fd5bbc6418dcd14f43d5c29f95f4aa554eaf1b3a
boxmein <wsdf294@gmail.com>
2014-08-29 16:15:43 +0300

    server now sends CORS accept headers for everything, also as a feature blocks .-paths

--- server.js
+++ server.js
@@ -3,10 +3,19 @@ var static = require('node-static');
 //
 // Create a node-static server instance to serve the './public' folder
 //
-var file = new static.Server('.');
+var file = new static.Server('.', {
+  'headers': {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, OPTIONS',
+  }
+});
 
 require('http').createServer(function (request, response) {
     request.addListener('end', function () {
+        if (request.url.match(/\/\./)) {
+          response.writeHead(403);
+          response.end('Unauthorized');
+        }
         file.serve(request, response);
     }).resume();
 }).listen(process.env.PORT || 3000);
